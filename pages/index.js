4a7012d87ca6ed335cb0c770eff5c170eaa2c3b7
Moritz Wright <me@moritzwright.com>
2022-04-02 19:23:26 +0200

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-import Image from "next/image";
 import Card from "../components/Card";
 import InformationModal from "../components/InformationModal";
 
 // components
 import Seo from "../components/Seo";
-
-// styles
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (

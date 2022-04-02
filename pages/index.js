21df499546b2041907cb45db30092b33513c8f87
Moritz Wright <me@moritzwright.com>
2022-04-02 17:20:47 +0200

    jdasko

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <main className={styles.container}>
-      <Seo title="Sarah Radtke | Privat Praxis für Physiotherapie | Hamburg" />
+      {/* <Seo title="Sarah Radtke | Privat Praxis für Physiotherapie | Hamburg" />
       <div className={styles.portrait}>
         <Image
           src="/img/sarah-radtke-portrait.jpeg"
@@ -25,7 +25,18 @@ export default function Home() {
           layout="fill"
           objectFit="contain"
         />
-      </div>
+      </div> */}
+
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "50px",
+          height: "50px",
+          background: "red",
+        }}
+      ></div>
     </main>
   );
 }
